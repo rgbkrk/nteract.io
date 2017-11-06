f0e851af0840be011adff1c0181c103b43720a1e
Kyle Kelley <rgbkrk@gmail.com>
2017-11-06 14:34:48 -0800

    create cell component for site use
    ...that totally rips off our current implementation

---
+++ pages/architecture.js
@@ -0,0 +1,158 @@
+// @flow
+import Layout from "../components/layout/layout";
+import {
+  ContentSection,
+  ContentSectionPane
+} from "../components/content-section/content-section";
+
+import React from "react";
+
+import {
+  PageHeader,
+  PageHeaderLeft,
+  PageHeaderRight
+} from "../components/page-header/page-header";
+
+type CellProps = {
+  executionCount: "*" | number | null
+};
+
+const Cell = () => {
+  return (
+    <div className="cell">
+      <div className="input-container">
+        <div className="prompt">[ ]</div>
+        <pre className="input">import this</pre>
+      </div>
+      <div>
+        <div className="outputs">
+          <pre>{`The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+          `}</pre>
+        </div>
+      </div>
+      <style jsx>{`
+        pre {
+          /* There are currently global styles that are overwriting <pre> to
+          have terrible defaults, complete with their own !important */
+          padding: 0px !important;
+          box-shadow: none;
+        }
+
+        div {
+          /* well this is neat, I can set the css variable inline */
+          --prompt-width: 50px;
+        }
+
+        .cell {
+          /* only for hover though */
+          box-shadow: 3px 3px 9px rgba(0, 0, 0, 0.12),
+            -3px -3px 9px rgba(0, 0, 0, 0.12);
+
+          position: relative;
+          background: var(--cell-bg);
+          transition: all 0.1s ease-in-out;
+        }
+
+        .input-container {
+          display: flex;
+          flex-direction: row;
+          padding: 0px;
+        }
+
+        .input {
+          flex: 1 1 auto;
+          overflow: auto;
+          font-family: "Source Code Pro", monospace;
+          letter-spacing: 0.3px;
+          word-spacing: 1px;
+          background-color: rgb(250, 250, 250);
+          font-size: 14px;
+          line-height: 20px;
+
+          position: relative;
+          top: 0px;
+          margin: 0 auto;
+          padding: 10px 0px 10px 10px !important;
+        }
+
+        .outputs {
+          padding: 10px 10px 10px calc(var(--prompt-width) + 10px);
+        }
+
+        .outputs pre {
+          white-space: pre-wrap;
+          font-size: 14px;
+          word-wrap: break-word;
+        }
+
+        .prompt {
+          font-family: monospace;
+          background-color: rgb(226, 223, 227);
+          color: rgb(140, 138, 142);
+          width: var(--prompt-width);
+          padding: 9px 0;
+          text-align: center;
+          flex: 0 0 auto;
+
+          letter-spacing: 0.3px;
+          word-spacing: 1px;
+          font-size: 14px;
+          line-height: 20px;
+
+          margin: 0 auto;
+          padding: 10px 0px 10px 0px !important;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+const SectionOne = () => (
+  <ContentSection>
+    <ContentSectionPane layout="left">
+      <h3>Foundations</h3>
+      <p>
+        nteract is built on top of a suite of protocols and file formats from
+        the jupyter project.
+      </p>
+      <Cell />
+    </ContentSectionPane>
+  </ContentSection>
+);
+
+export default class AboutPage extends React.Component<OSProps, void> {
+  render() {
+    let themeColor = "#333333";
+
+    return (
+      <Layout pageTitle=": We're people, not software!" themeColor={themeColor}>
+        <PageHeader themeColor={themeColor}>
+          <PageHeaderLeft>
+            <h1>Architecture</h1>
+          </PageHeaderLeft>
+        </PageHeader>
+        <SectionOne />
+      </Layout>
+    );
+  }
+}
